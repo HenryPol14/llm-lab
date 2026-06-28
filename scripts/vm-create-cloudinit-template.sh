d716ff094f234641b203a8f9d421384757722a8d
GennadiSer <henrynik@gmail.com>
2026-06-28 22:51:57 +0300

    Add openssh-server to cloud image template

--- scripts/vm-create-cloudinit-template.sh
+++ scripts/vm-create-cloudinit-template.sh
@@ -34,8 +34,9 @@ if [[ ! -f "$PREPARED_IMAGE_PATH" || "${FORCE_REBUILD:-0}" == "1" ]]; then
   info "Preparing cloud image with guest packages: ${PREPARED_IMAGE_PATH}"
   cp "$UBUNTU_IMAGE_PATH" "$PREPARED_IMAGE_PATH"                    # копируем образ для подготовки шаблона
   virt-customize -a "$PREPARED_IMAGE_PATH" \
-    --install qemu-guest-agent,cloud-init,cloud-initramfs-dyn-net-conf,iproute2,ssh,curl,git,jq,nvtop,pciutils,cloud-guest-utils,gdisk,parted,ca-certificates,gnupg,lsb-release,ubuntu-drivers-common,rsync \
+    --install qemu-guest-agent,cloud-init,cloud-initramfs-dyn-net-conf,iproute2,openssh-server,ssh,curl,git,jq,nvtop,pciutils,cloud-guest-utils,gdisk,parted,ca-certificates,gnupg,lsb-release,ubuntu-drivers-common,rsync \
     --run-command 'systemctl enable qemu-guest-agent' \
+    --run-command 'systemctl enable ssh' \
     --run-command 'systemctl enable cloud-init' \
     --run-command 'systemctl enable cloud-config' \
     --run-command 'systemctl enable cloud-final' \
