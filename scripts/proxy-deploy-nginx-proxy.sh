52d9ccbac448af1a666113cf205047ea6f574839
GennadiSer <henrynik@gmail.com>
2026-06-29 21:00:11 +0300

    fix(proxy): Add proxy_redirect for Grafana path rewriting

--- scripts/proxy-deploy-nginx-proxy.sh
+++ scripts/proxy-deploy-nginx-proxy.sh
@@ -197,6 +197,7 @@ server {
     # Grafana
     location /grafana/ {
         proxy_pass         http://${MONITORING_IP}:3000/;
+        proxy_redirect   / /grafana/;
         proxy_set_header   Host \$host;
         proxy_set_header   X-Real-IP \$remote_addr;
         proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
